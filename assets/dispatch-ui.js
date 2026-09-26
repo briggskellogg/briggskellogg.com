ba4ee94aa17cbcb87ed7f772945cc0068cd30422
briggskellogg <briggs@kellogg.co>
2026-09-25 19:47:23 -0700

    Try the new transparent Instinct Logic and Psyche charms

--- assets/dispatch-ui.js
+++ assets/dispatch-ui.js
@@ -1,7 +1,7 @@
 (function() {
     'use strict';
 
-    function blob(arch) { return '/assets/blobs/' + ({instinct:'slash-instinct',logic:'build-logic',psyche:'nest-psyche'}[arch] || 'build-logic') + '-charm.webp'; }
+    function blob(arch) { return '/assets/blobs/' + ({instinct:'instinct',logic:'logic',psyche:'psyche'}[arch] || 'logic') + '-charm-v2.webp'; }
 
     function pad2(n) { return (n < 10 ? '0' : '') + n; }
 
